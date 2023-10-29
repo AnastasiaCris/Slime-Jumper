52bfdb79734515d2fbee4aca6e101702063b9adc
Morran25 <longwinter25@gmail.com>
2023-10-29 20:08:17 +0100

    Moving all the progress to the Hub

---
+++ Slime Jumper/Assets/Scripts/Player.cs
@@ -0,0 +1,172 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class Player : MonoBehaviour
+{
+    [Header("Player Elements")]
+    Vector2 moveImput;
+    Rigidbody2D myRigidbody2D;
+    Animator myAnimator;
+
+    CapsuleCollider2D myBodyCollider;
+
+    BoxCollider2D legCollider;
+
+    [SerializeField] float myTimer;
+
+
+
+    [Header("Player Movement")]
+    [SerializeField] float playerSpeed = 10;
+    [SerializeField] float jumpPower = 10;
+
+    [SerializeField] float climbingSpeed = 5f;
+
+    [SerializeField] List<Animation> attackAnimations;
+
+
+    float startingGravity;
+
+    int attackCounter = 0;
+    float attackTimer = 0;
+
+
+    [SerializeField] float comboDeactivation = 2;
+    void Start()
+    {
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myBodyCollider = GetComponent<CapsuleCollider2D>();
+        legCollider = GetComponent<BoxCollider2D>();
+
+
+        startingGravity = myRigidbody2D.gravityScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        FlipSprite();
+
+        ComboTimer();
+
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveImput = value.Get<Vector2>();
+        myAnimator.SetBool("IsAttacking", false);
+    }
+    void Move()
+    {
+        Vector2 playerVelocity = new Vector2(moveImput.x * playerSpeed, myRigidbody2D.velocity.y);
+        myRigidbody2D.velocity = playerVelocity;
+
+        bool playerMovesOnX = Mathf.Abs(myRigidbody2D.velocity.x) > Mathf.Epsilon;
+
+        myAnimator.SetBool("IsRunning", playerMovesOnX);
+
+
+
+    }
+
+    void FlipSprite()
+    {
+
+        bool playerMovesOnX = Mathf.Abs(myRigidbody2D.velocity.x) > Mathf.Epsilon; //smart way of saying movement on x > 0
+
+        if (playerMovesOnX)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody2D.velocity.x), 1f); //Using Mathf.Sign set 0 value as positive,
+                                                                                          // causing the player to flip to the right
+        }
+    }
+    void OnFire(InputValue value)
+    {
+
+        // add + 1 to attack counter
+        attackCounter += 1;
+
+        UnityEngine.Debug.Log(attackTimer);
+
+        if (!myAnimator.GetBool("IsAttacking"))
+        {
+
+            attackCounter = 0;
+            attackTimer = 2;
+            myAnimator.SetInteger("AttackCounter", attackCounter);
+
+            myAnimator.SetBool("IsAttacking", true);
+            UnityEngine.Debug.Log("Combo has been started");
+
+
+        }
+
+        if (myAnimator.GetBool("IsAttacking") && attackCounter != 3)
+        {
+            attackTimer += 1;
+            myAnimator.SetInteger("AttackCounter", attackCounter);
+            UnityEngine.Debug.Log(attackCounter);
+
+
+        }
+
+        else if (myAnimator.GetBool("IsAttacking") && attackCounter == 3)
+        {
+            attackCounter = 0;
+            attackTimer += 1;
+            UnityEngine.Debug.Log("attack timer is " + attackTimer);
+
+            myAnimator.SetInteger("AttackCounter", attackCounter);
+
+
+        }
+
+
+
+        // if attack counter > 3 reset back to 0
+        // if input was not pressed for two seconds set bool to false
+
+    }
+
+    IEnumerator DeactivateCombo()
+    {
+
+        yield return new WaitForSecondsRealtime(comboDeactivation);
+        myAnimator.SetBool("IsAttacking", false);
+        attackCounter = 0;
+
+    }
+
+    void ComboCheck()
+    {
+
+
+
+        if (myAnimator.GetBool("IsAttacking") && attackTimer < 0)
+        {
+
+            StartCoroutine(DeactivateCombo());
+            UnityEngine.Debug.Log("Combo has been reset");
+        }
+
+    }
+
+    void ComboTimer()
+    {
+        while (attackTimer > 0)
+        {
+            attackTimer -= 1;
+            UnityEngine.Debug.Log("attack timer is " + attackTimer);
+        }
+
+        ComboCheck();
+    }
+
+}
